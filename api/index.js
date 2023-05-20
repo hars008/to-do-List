7f7c184eb1a2bd220bed3322fbbeb8c8daef12d9
hars699 <harshbansal699@gmail.com>
2023-05-20 09:55:15 +0530

    deployments bugs

--- api/index.js
+++ api/index.js
@@ -1,9 +1,7 @@
-
-const express = require('express')
-const cors= require("cors")
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const fs = require("fs");
-
 
 // var data=fs.readFileSync("./todos.json", "utf8", function (err, data) {
 //   if (err) throw err;
@@ -18,7 +16,7 @@ var users = JSON.parse(fs.readFileSync("./user.json", "utf8"));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://keen-muffin-b24056.netlify.app/",
     credentials: true,
   })
 );
@@ -26,22 +24,21 @@ app.use(function (req, res, next) {
   res.setHeader("Content-Type", "application/json");
   next();
 });
-app.get("/", (req,res)=>{
-    res.json("sjfsk")
-})
-app.get("/todos" , (req,res)=>{
-    // console.log(todos)
-    res.json(data);
-    // res.sendFile(path.join(__dirname,  "todos.json"));
-})
-app.get("/users" , (req, res)=>{
-    res.json(users);
-})
-app.get("/users/:id" , (req,res)=>{
-    var idz= req.params.id;
-    var user= users.find((user)=> user.id==idz);
-    res.json(user);
-}
-)
+app.get("/", (req, res) => {
+  res.json("sjfsk");
+});
+app.get("/todos", (req, res) => {
+  // console.log(todos)
+  res.json(data);
+  // res.sendFile(path.join(__dirname,  "todos.json"));
+});
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+app.get("/users/:id", (req, res) => {
+  var idz = req.params.id;
+  var user = users.find((user) => user.id == idz);
+  res.json(user);
+});
 
 app.listen(1234);
